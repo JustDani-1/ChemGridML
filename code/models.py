2832904a72e7b91a054c35e707dfcac0c372057f
= <=>
2025-09-01 22:09:43 +0100

    altered the FNN

--- code/models.py
+++ code/models.py
@@ -80,18 +80,18 @@ class FNN(BaseModel, torch.nn.Module):
     
     @staticmethod
     def get_hyperparameter_space(trial):
-        # commented out the big hyperparams for development
-        # return {
-        #     # Model hyperparameters
-        #     'hidden_sizes': trial.suggest_categorical('hidden_sizes', [[128], [128, 64], [256, 128, 64]]),
-        #     'dropout_rate': trial.suggest_categorical('dropout_rate', [0.0, 0.1, 0.25, 0.4, 0.5]),
-        #     'activation': trial.suggest_categorical('activation', ['relu', 'tanh']),
+        #commented out the big hyperparams for development
+        return {
+            # Model hyperparameters
+            'hidden_sizes': trial.suggest_categorical('hidden_sizes', [[128], [128, 64], [256, 128, 64]]),
+            'dropout_rate': trial.suggest_categorical('dropout_rate', [0.0, 0.1, 0.25, 0.4, 0.5]),
+            'activation': trial.suggest_categorical('activation', ['relu', 'tanh']),
             
-        #     # Training hyperparameters
-        #     'epochs': trial.suggest_categorical('epochs', [100, 150, 200]),
-        #     'lr': trial.suggest_float('lr', 1e-5, 1e-2, log=True),
-        #     'batch_size': trial.suggest_categorical('batch_size', [16, 32, 64])
-        # }
+            # Training hyperparameters
+            'epochs': trial.suggest_categorical('epochs', [100, 150, 200]),
+            'lr': trial.suggest_float('lr', 1e-5, 1e-2, log=True),
+            'batch_size': trial.suggest_categorical('batch_size', [16, 32, 64])
+        }
         return {
             # Model hyperparameters
             'hidden_sizes': trial.suggest_categorical('hidden_sizes', [[32], [32, 16], [32, 16, 8]]),
