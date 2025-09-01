05be236470848e110d369322ed3d81942e73c97a
= <=>
2025-09-01 11:56:35 +0100

    small changes to array job

--- scripts/submit_array.sh
+++ scripts/submit_array.sh
@@ -3,7 +3,7 @@
 # Batch script to run a serial array job under SGE.
 
 # Request ten minutes of wallclock time (format hours:minutes:seconds).
-#$ -l h_rt=0:20:0
+#$ -l h_rt=0:30:0
 
 # Request 1 gigabyte of RAM (must be an integer followed by M, G, or T)
 #$ -l mem=2G
@@ -21,6 +21,4 @@ conda activate internship
 
 python ./code/main.py $JOB_ID $SGE_TASK_ID
 
-mkdir ./output/$JOB_ID
-
-mv "${JOB_NAME}"* ./output/$JOB_ID
+mkdir -p ./output/$JOB_ID
