10c22ab10d92d1236a7872c3fd1b742f91b9ea67
= <=>
2025-09-19 10:46:20 +0100

    environment changes

--- code/features.py
+++ code/features.py
@@ -6,24 +6,42 @@ import env
 import numpy as np
 import deepchem as dc
 
+def generate(mols, gen):
+    fingerprints = []
+    failed_count = 0
+    
+    for mol in mols:
+        try:
+            if mol is not None:
+                fp = gen.GetFingerprint(mol)
+                fingerprints.append(np.array(fp))
+            else:
+                failed_count += 1
+        except Exception as e:
+            failed_count += 1
+    
+    print(f"Feature failed for {failed_count} molecules")
+    
+    return np.stack(fingerprints)
+
 def AtomPair(mols, fpSize=env.DEFAULT_FP_SIZE):
     gen = rdFingerprintGenerator.GetAtomPairGenerator(includeChirality=True, fpSize=fpSize)
-    return np.stack([np.array(gen.GetFingerprint(x)) for x in mols])
+    return generate(mols, gen)
 
 def ECFP(mols, radius=2, fpSize=env.DEFAULT_FP_SIZE):
     gen = rdFingerprintGenerator.GetMorganGenerator(includeChirality=True, radius=radius, fpSize=fpSize)
-    return np.stack([np.array(gen.GetFingerprint(x)) for x in mols])
+    return generate(mols, gen)
 
 def MACCS(mols):
     return np.stack([np.array(MACCSkeys.GenMACCSKeys(x)) for x in mols])
 
 def RDKitFP(mols, fpSize=env.DEFAULT_FP_SIZE):
     gen = rdFingerprintGenerator.GetRDKitFPGenerator(fpSize=fpSize)
-    return np.stack([np.array(gen.GetFingerprint(x)) for x in mols])
+    return generate(mols, gen)
 
 def TOPOTOR(mols, fpSize=env.DEFAULT_FP_SIZE):
     gen = rdFingerprintGenerator.GetTopologicalTorsionGenerator(includeChirality=True, fpSize=fpSize)
-    return np.stack([np.array(gen.GetFingerprint(x)) for x in mols])
+    return generate(mols, gen)
 
 def MOL2VEC(mols):
     return dc.deepchem.feat.Mol2VecFingerprint().featurize(mols)
@@ -41,4 +59,6 @@ def getFeature(mols, fingerprint: str):
         raise ValueError(f"{fingerprint} is not a callable function")
     
     return func(mols)
+
+
 
